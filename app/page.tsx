ffdeb9c11efd8295a7b65708a7cee89aa447702d
Karoll Escalante <karollescalante02@gmail.com>
2025-03-18 11:53:04 -0500

    Add LatestVideos and Testimonials sections, and include Footer in the Home component for enhanced content presentation.

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,20 @@ import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Features from "./sections/Feautures";
+import Footer from "./sections/Footer";
+import LatestVideos from "./sections/LatestVideos";
+import Testimonials from "./sections/Testimonials";
 export default function Home() {
   return (
-    <div className="relative min-h-screen overflow-hidden px-4 md:px-20 lg:px-22">
+    <div className="relative min-h-screen overflow-hidden px-4 md:px-20 lg:px-22 ">
       
       {/* <Header /> */}
       <Hero />
       <About />
       <Features />
+      <LatestVideos />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
